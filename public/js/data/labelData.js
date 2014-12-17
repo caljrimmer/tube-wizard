26bd89902cd167fccb42064f77b807051a72bdb6
Callum Rimmer <callum@deadtrendy.co.uk>
2014-12-17 13:28:43 +0000

    D3 all native

---
+++ public/js/data/labelData.js
@@ -0,0 +1,92 @@
+define([], function() {  
+   
+	var Labels = {
+    	circle: {
+			N: {
+				text: {
+					top: -18,
+					left: -8
+				},
+				label: {
+					top: -30,
+					left: -15
+				}
+			},
+			S: {
+				text: {
+					top: 26,
+					left: -10
+				},
+				label: {
+					top: 14,
+					left: -18
+				}
+			},
+			W: {
+				text: {
+					top: 4,
+					left: -44
+				},
+				label: {
+					top: -8,
+					left: -50
+				}
+			},
+			E: {
+				text: {
+					top: 4,
+					left: 22
+				},
+				label: {
+					top: -8,
+					left: 15
+				}
+			}
+		},
+		rect: {
+			N: {
+				text: {
+					top: -18,
+					left: -8
+				},
+				label: {
+					top: -30,
+					left: -15
+				}
+			},
+			S: {
+				text: {
+					top: 26,
+					left: -10
+				},
+				label: {
+					top: 14,
+					left: -18
+				}
+			},
+			W: {
+				text: {
+					top: 4,
+					left: -44
+				},
+				label: {
+					top: -8,
+					left: -50
+				}
+			},
+			E: {
+				text: {
+					top: 4,
+					left: 22
+				},
+				label: {
+					top: -8,
+					left: 15
+				}
+			}
+		}
+	}
+	
+	return Labels;
+	
+});
